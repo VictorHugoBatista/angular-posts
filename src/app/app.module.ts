56dd3c2ff658262d4b1cb75f4cede3173e1f0e60
Victor Hugo <vct.bta@gmail.com>
2018-01-27 12:13:34 -0200

    Importa o módulo http da aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -18,7 +19,8 @@ import { SinglePostComponent } from './single-post/single-post.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [PostsService],
   bootstrap: [AppComponent]
