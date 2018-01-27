181c4d2b6c8f2be45618c756955d97fe42e31c4b
Victor Hugo <vct.bta@gmail.com>
2018-01-27 11:07:02 -0200

    Adiciona o componente list-posts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { ListPostsComponent } from './list-posts/list-posts.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListPostsComponent,
   ],
   imports: [
     BrowserModule,
