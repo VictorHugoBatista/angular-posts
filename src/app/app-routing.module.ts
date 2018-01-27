dedf781eb40ef813af25c78c5c6c5ad832d55902
Victor Hugo <vct.bta@gmail.com>
2018-01-27 11:08:09 -0200

    Cria as rotas /posts e /post

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListPostsComponent } from './list-posts/list-posts.component';
+import { SinglePostComponent } from './single-post/single-post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'posts', component: ListPostsComponent },
+  { path: 'post', component: SinglePostComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
