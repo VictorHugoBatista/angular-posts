b29b0fd5f782fb887a602093a4e9634bff82ac70
Victor Hugo <vct.bta@gmail.com>
2018-01-28 12:19:57 -0200

    Substitui evento input do campo de pesquisa por método set

--- src/app/list-posts/list-posts.component.ts
+++ src/app/list-posts/list-posts.component.ts
@@ -9,10 +9,15 @@ import { PostsService } from '../posts.service';
 export class ListPostsComponent implements OnInit {
 
   private posts;
-  searchText = '';
+  private _searchText = '';
   private pageNumber: number;
 
   constructor(private postsService: PostsService) {}
+
+  set searchText(searchText: string) {
+    this._searchText = searchText;
+    this.updateList(true);
+  }
 
   ngOnInit() {
     this.updateList(true);
@@ -22,7 +27,8 @@ export class ListPostsComponent implements OnInit {
     if (overwriteList) {
       this.pageNumber = 0;
     }
-    this.postsService.getPosts(this.searchText, ++this.pageNumber)
+    this.postsService
+      .getPosts(this._searchText, ++this.pageNumber)
       .subscribe(data => {
         if (overwriteList) {
           this.posts = data;
